84546c3f4de2e329c9ae9c10a1be91f339be974f
ddangmul <j.ddangmul@gmail.com>
2024-12-07 14:57:11 +0900

    [상태 구문 수정] 질문 배열 인덱스 상태를 사용자 응답 배열 상태에서 파생

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,11 +1,12 @@
 import { useState } from "react";
 
+import QUESTIONS from "../questions.js";
+
 export default function Quiz() {
-  // 질문 배열의 인덱스를 관리
-  const [activeQuestionIndex, setActiveQuestionIndex] = useState(0);
   // 사용자 응답을 배열에 저장
   const [useAnswers, setUserAnswer] = useState([]);
+  const activeQuestionIndex = useAnswers.length;
 
   return <p>Currently active Question</p>;
 }
-``
+``;
