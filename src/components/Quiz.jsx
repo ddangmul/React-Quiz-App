4486b4e61ee5fa04d65ba162746e47468b9fc43b
ddangmul <j.ddangmul@gmail.com>
2024-12-07 22:16:51 +0900

    답변 셔플 & 퀴즈 종료 시 대체 내용 렌더링

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,11 +1,15 @@
 import { useState } from "react";
 
 import QUESTIONS from "../questions.js";
+import quizCompleteImg from "../assets/quiz-complete.png";
 
 export default function Quiz() {
   // 사용자 응답을 배열에 저장
   const [userAnswers, setUserAnswer] = useState([]);
+
   const activeQuestionIndex = userAnswers.length;
+  // 모든 퀴즈 답변 완료 상황 (활성화된 질문 인덱스와 원본 질문 배열 길이가 일치)
+  const quizIsComplete = activeQuestionIndex === QUESTIONS.length;
 
   function handleSelectAnswer(selectedAnswer) {
     setUserAnswer((prevAnswers) => {
@@ -13,12 +17,27 @@ export default function Quiz() {
     });
   }
 
+  // [모든 퀴즈 답변 완료 시] 대체 내용 렌더링
+  if (quizIsComplete) {
+    return (
+      <div id="summary">
+        <img src={quizCompleteImg} alt="Trophy icon" />
+        <h2>Quiz Complete!</h2>
+      </div>
+    );
+  }
+
+  // [답변 셔플]
+  // 위치 주의: quizIsComplete 상황이 아닐 경우(if문 퀴즈 답변 완료 시 렌더링 구문 이후)에 작성해야 로직 작동
+  const shuffledAnswers = [...QUESTIONS[activeQuestionIndex].answers];
+  shuffledAnswers.sort(() => Math.random() - 0.5); // sort() : 새 배열 반환하지 않고 원본 배열을 수정
+
   return (
     <div id="quiz">
       <div id="question">
         <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
         <ul id="answers">
-          {QUESTIONS[activeQuestionIndex].answers.map((answer) => (
+          {shuffledAnswers.map((answer) => (
             <li key={answer} className="answer">
               <button onClick={() => handleSelectAnswer(answer)}>
                 {answer}
