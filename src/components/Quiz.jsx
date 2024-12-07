79d940ecc255c829b149641eb9ca4f631c1b1276
ddangmul <j.ddangmul@gmail.com>
2024-12-07 21:54:30 +0900

    질문&답변 렌더링

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -4,9 +4,29 @@ import QUESTIONS from "../questions.js";
 
 export default function Quiz() {
   // 사용자 응답을 배열에 저장
-  const [useAnswers, setUserAnswer] = useState([]);
-  const activeQuestionIndex = useAnswers.length;
+  const [userAnswers, setUserAnswer] = useState([]);
+  const activeQuestionIndex = userAnswers.length;
 
-  return <p>Currently active Question</p>;
+  function handleSelectAnswer(selectedAnswer) {
+    setUserAnswer((prevAnswers) => {
+      return [...prevAnswers, selectedAnswer]; // 추가된 답변 배열을 setUserAnswer가 반환(상태 업데이트)하도록 return 명시
+    });
+  }
+
+  return (
+    <div id="quiz">
+      <div id="question">
+        <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
+        <ul id="answers">
+          {QUESTIONS[activeQuestionIndex].answers.map((answer) => (
+            <li key={answer} className="answer">
+              <button onClick={() => handleSelectAnswer(answer)}>
+                {answer}
+              </button>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
 }
-``;
