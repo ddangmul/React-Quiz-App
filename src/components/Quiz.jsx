d982b39600506602ed3da51db38f235db62eac2d
ddangmul <j.ddangmul@gmail.com>
2024-12-09 00:55:36 +0900

    퀴즈 결과 렌더링: 답변 내역 & 결과 퍼센티지

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,8 +1,8 @@
 import { useState, useCallback } from "react";
 
 import QUESTIONS from "../questions.js";
-import quizCompleteImg from "../assets/quiz-complete.png";
 import Question from "./Question.jsx";
+import Summary from "./Summary.jsx";
 
 export default function Quiz() {
   // 사용자 응답을 배열에 저장
@@ -30,12 +30,7 @@ export default function Quiz() {
 
   // [모든 퀴즈 답변 완료 시] 대체 내용 렌더링
   if (quizIsComplete) {
-    return (
-      <div id="summary">
-        <img src={quizCompleteImg} alt="Trophy icon" />
-        <h2>Quiz Complete!</h2>
-      </div>
-    );
+    return <Summary userAnswers={userAnswers} />;
   }
 
   return (
